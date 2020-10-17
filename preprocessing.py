392e6de897bd13475f89506caca99511fb44ce68
Eric Mesina <esmesina02@gmail.com>
2020-10-17 15:58:02 -0400

    Switched to use datetime indexing for the final average series

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 east_west = pd.read_csv("east-west.csv", index_col=0, skiprows=1, nrows=24)
 west_east = pd.read_csv("west-east.csv", index_col=0, skiprows=1, nrows=24)
@@ -7,8 +8,6 @@ south_north = pd.read_csv("south-north.csv", index_col=0, skiprows=1, nrows=24)
 
 all_data = pd.concat([east_west, west_east, north_south, south_north], 1)
 
-average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns], index=east_west.columns)
+average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns], index=east_west.columns).stack()
 
-"""for col in east_west.columns:
-    print(col)
-    print(all_data[col].mean(1))"""
+average_data.index = [datetime.strptime(" ".join(i).strip(), "%m/%d/%Y %H:%M:%S") for i in average_data.index]
