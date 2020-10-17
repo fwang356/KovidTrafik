20d13da98bb05f955f2f06aac2804ce488da2572
Eric Mesina <esmesina02@gmail.com>
2020-10-17 17:22:13 -0400

    Modularized preprocessing and added folder to contain algorithm test results

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from datetime import datetime
 
-east_west = pd.read_csv("east-west.csv", index_col=0, skiprows=1, nrows=24)
-west_east = pd.read_csv("west-east.csv", index_col=0, skiprows=1, nrows=24)
-north_south = pd.read_csv("north-south.csv", index_col=0, skiprows=1, nrows=24)
-south_north = pd.read_csv("south-north.csv", index_col=0, skiprows=1, nrows=24)
+def get_data():
+    east_west = pd.read_csv("east-west.csv", index_col=0, skiprows=1, nrows=24)
+    west_east = pd.read_csv("west-east.csv", index_col=0, skiprows=1, nrows=24)
+    north_south = pd.read_csv("north-south.csv", index_col=0, skiprows=1, nrows=24)
+    south_north = pd.read_csv("south-north.csv", index_col=0, skiprows=1, nrows=24)
 
-all_data = pd.concat([east_west, west_east, north_south, south_north], 1)
+    all_data = pd.concat([east_west, west_east, north_south, south_north], 1)
 
-average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns], index=east_west.columns).stack()
+    average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns], index=east_west.columns).stack()
 
-average_data.index = [datetime.strptime(" ".join(i).strip(), "%m/%d/%Y %H:%M:%S") for i in average_data.index]
+    average_data.index = [datetime.strptime(" ".join(i).strip(), "%m/%d/%Y %H:%M:%S") for i in average_data.index]
+    
+    return average_data
