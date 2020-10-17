a46bbcbce876d10444f95632f4b1310b9f1dfa20
Eric Mesina <esmesina02@gmail.com>
2020-10-17 14:39:50 -0400

    Included date indices in averaged data frame

--- average_data.py
+++ average_data.py
@@ -7,7 +7,7 @@ south_north = pd.read_csv("south-north.csv", index_col=0, skiprows=1, nrows=24)
 
 all_data = pd.concat([east_west, west_east, north_south, south_north], 1)
 
-average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns])
+average_data = pd.DataFrame(data=[all_data[col].mean(1) for col in east_west.columns], index=east_west.columns)
 
 """for col in east_west.columns:
     print(col)
