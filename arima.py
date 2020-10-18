240a1fe492fc3d1015be63ceec592644ec4e0160
Felix Wang <fwang356@gatech.edu>
2020-10-18 01:41:18 -0400

    fixed files

--- arima.py
+++ arima.py
@@ -1,9 +1,9 @@
+import warnings
 import numpy as np
-import pandas.plotting as pd
-from pandas import Series
+# import pandas.plotting as pd
+import pandas as pd
 from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA
-import statsmodels.tsa.statespace.sarimax as SARIMAX
 from sklearn.metrics import mean_squared_error
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
@@ -15,15 +15,16 @@ def difference(dataset, interval=1):
 	for i in range(interval, len(dataset)):
 		value = dataset[i] - dataset[i - interval]
 		diff.append(value)
-	return Series(diff)
+	return pd.Series(diff)
 
 # invert differenced value
 def inverse_difference(history, yhat, interval=1):
 	return yhat + history[-interval]
 
+warnings.filterwarnings("ignore")
 series = pp.get_data()
 X = series.values
-size = int(len(X) * 0.66)
+size = int(len(X) * 0.85)
 train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
@@ -33,7 +34,7 @@ for i in range(len(test)):
 	hours_in_week = 168
 	diff = difference(history, hours_in_week)
 	# predict
-	model = ARIMA(diff, order=(2, 1, 1))
+	model = ARIMA(diff, order=(1, 0, 0))
 	model_fit = model.fit(trend='nc', disp=0)
 	yhat = model_fit.forecast()[0]
 	yhat = inverse_difference(history, yhat, hours_in_week)
@@ -45,9 +46,13 @@ for i in range(len(test)):
 error = mean_squared_error(test, predictions)
 print('Test MSE: %.3f' % error)
 
-"""
+residuals = [test[i]-predictions[i] for i in range(len(test))]
+residuals = pd.DataFrame(residuals)
+print(residuals.describe())
 # plot
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
+pyplot.figure()
+pyplot.subplot(211)
+residuals.hist(ax=pyplot.gca())
+pyplot.subplot(212)
+residuals.plot(kind='kde', ax=pyplot.gca())
 pyplot.show()
-"""
