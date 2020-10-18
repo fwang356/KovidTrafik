5f79aec70bc51e9c7d1b4e4127f7df23e2184ba1
Felix Wang <fwang356@gatech.edu>
2020-10-18 07:00:24 -0400

    finish

--- forecast.py
+++ forecast.py
@@ -1,32 +1,33 @@
+from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA, ARIMAResults
+from sklearn.metrics import mean_squared_error
 import preprocessing as pp
 from arima import difference, inverse_difference
 
-
 def predict(hours):
-    series = pp.get_total()
-    X = series.values.astype('float32')
+    series = pp.get_data()
+    X = series.values
     history = [x for x in X]
     hours_in_week = 168
+    validation = pp.get_validate()
+    y = validation.values
 
     model_fit = ARIMAResults.load('model.pkl')
 
     predictions = list()
-
     yhat = model_fit.forecast()[0]
     yhat = inverse_difference(history, yhat, hours_in_week)
     predictions.append(yhat)
-    history.append(yhat)
+    history.append(yhat[0])
 
     for i in range(1, hours):
-        hours_in_week = 168
         diff = difference(history, hours_in_week)
 
-        model = ARIMA(diff, order=(3, 1, 1))
+        model = ARIMA(diff, order=(3,1,1))
         model_fit = model.fit(trend='nc', disp=0)
         yhat = model_fit.forecast()[0]
         yhat = inverse_difference(history, yhat, hours_in_week)
-        history.append(yhat)
+        history.append(yhat[0])
         predictions.append(yhat)
 
-    return history
+    return predictions
--- kovidtrafik.py
+++ kovidtrafik.py
@@ -8,35 +8,33 @@ from forecast import predict
 warnings.filterwarnings("ignore")
 series = pp.get_total()
 
-
 def mean(dataset):
-    average = sum(dataset) / len(dataset)
+    average = dataset.sum() / dataset.size
     return average
-
 
 def model(date, time):
     aug30 = dt.date(2020, 8, 30)
     delta = date - aug30
     hours = (delta.days - 1) * 24 + time.hour + 1
-    history = predict(hours)
+    predictions = predict(hours)
 
-    # predictionsdf = pd.DataFrame(predictions)
-    # total_data = series.append(predictionsdf)
-    average = mean(history)
-    target = history[-1]
-    percent = 100 * (target / average)
+    predictionsdf = pd.DataFrame(predictions)
+    total_data = series.append(predictionsdf)
+    average = mean(total_data)
+    target = predictions[hours - 1]
+    percent = (target / average)
 
-    upper = np.percentile(np.ndarray(history), 65)
-    lower = np.percentile(np.ndarray(history), 35)
+    upper = np.percentile(total_data, 65)
+    lower = np.percentile(total_data, 35)
 
     if target < lower:
-        print("Traffic levels in Atlanta will be low, about %d percent of the average" % percent)
+        print("Traffic levels in Atlanta will be low, about %.2f times the average" % percent)
     elif target > upper:
-        print("Traffic levels in Atlanta will be high, about %d percent of the average" % percent)
+        print("Traffic levels in Atlanta will be high, about %.2f times the average" % percent)
     else:
-        print("Traffic levels in Atlanta will be moderate, about %d percent of the average" % percent)
-
+        print("Traffic levels in Atlanta will be moderate, about %.2f times the average" % percent)
 
 date = dt.date(2020, 8, 31)
-time = dt.time(1)
+time = dt.time(6)
+
 model(date, time)
